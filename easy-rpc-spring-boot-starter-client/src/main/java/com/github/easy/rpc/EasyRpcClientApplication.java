3a9ae482dfd10b919c6cf491e8381c11d2219762
zhuhai <zhuhai@corp.netease.com>
2024-01-04 20:48:37 +0800

    【easy-rpc】更改包名

--- easy-rpc-spring-boot-starter-client/src/main/java/com/github/easy/rpc/EasyRpcClientApplication.java
+++ easy-rpc-spring-boot-starter-client/src/main/java/com/github/easy/rpc/EasyRpcClientApplication.java
@@ -3,13 +3,11 @@ package com.github.easy.rpc;
 import com.github.easy.rpc.annotation.EasyRpcScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author zhuhai
  * @date 2023/12/26
  */
-@EnableEurekaClient
 @EasyRpcScan
 @SpringBootApplication
 public class EasyRpcClientApplication {
